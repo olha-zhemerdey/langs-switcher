079054b850947e011c5a1c793c9ff3b06fd61ead
Olha <oyashnaya88@gmail.com>
2024-12-15 23:55:34 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -81,6 +81,61 @@ export default function App() {
           Proceed
         </button>
       </div>
+
+      {/* Логін форма */}
+      <LoginForm />
     </div>
   );
 }
+
+const LoginForm = () => {
+  const [values, setValues] = useState({
+    login: "",
+    password: "",
+  });
+
+  const handleChange = (evt) => {
+    setValues({
+      ...values,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const handleSumit = (evt) => {
+    evt.preventDefault();
+    console.log(values);
+    setValues({
+      login: "",
+      password: "",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSumit} className={css.loginForm}>
+      <h2>Login Form</h2>
+      <div>
+        <label>
+          Login:
+          <input
+            type="text"
+            name="login"
+            value={values.login}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
